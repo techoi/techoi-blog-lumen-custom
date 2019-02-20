0ce51abe9b48635dbcee676e2b1b5b6ac1101c77
scurry <scurrymode@gmail.com>
2019-02-21 00:17:02 +0900

    Initial

---
+++ config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  url: 'https://techoi.github.io',
+  title: 'As long as we dare to dream and do not get in the way of ourseleves, anything is possible',
+  subtitle: 'Read, Inspired and Write.',
+  copyright: '© 2019 techoi All rights reserved.',
+  disqusShortname: 'techoi',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-134636751-1',
+  menu: [
+    {
+      label: 'Posts',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Jimmy Choi (techoi)',
+    photo: '/photo.jpg',
+    bio: 'Frontend Engineer, based on Sales, Business Development and Econimics.',
+    contacts: {
+      email: 'techoi77@gmail.com',
+      linkedin: 'https://www.linkedin.com/in/choi-jimmy-640b1a17b',
+      twitter: '#',
+      github: 'techoi',
+      rss: '#',
+      vkontakte: '#'
+    }
+  }
+};
---
+++ gatsby-browser.js
@@ -0,0 +1,7 @@
+'use strict';
+
+require('./src/assets/scss/init.scss');
+
+require('./src/assets/css/prism-okaidia-custom.css');
+
+exports.onClientEntry = () => {};
