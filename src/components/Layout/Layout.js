6d82da93c091f31606d4b525f11f18a32b4b1b06
techoi <techoi@github.com>
2019-07-02 12:56:44 +0900

    feat: google adsense 추가 (google analytics & google search console & google adsense)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,6 +8,16 @@ const Layout = ({ children, title, description }) => (
       <html lang="en" />
       <title>{title}</title>
       <meta name="description" content={description} />
+      {/* Google Search Console */}
+      <meta name="google-site-verification" content="HTZyqitny3N_5Zoo2r3UvFf44IvBf44s96xC1AU8YFI" />
+      {/* Google Adsense */}
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <script>
+        {`(adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-3091812896982510",
+          enable_page_level_ads: true
+        });`}
+      </script>
     </Helmet>
     {children}
   </div>
